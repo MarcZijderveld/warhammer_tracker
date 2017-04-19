0e3d968a574cf3c2d1595607672e94b9e94bc10d
Marc Zijderveld <marczijderveld@planet.nl>
2017-04-19 14:04:23 +0200

    Added gitignore and working color picker

---
+++ Warhammer_Tracker/Assets/Scripts/UnitInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitType {
+	Phoenix_Guard,
+	Sisters_of_Avelorn,
+    Swordmasters_of_Hoeth,
+    Silver_Helms,
+    Shadow_Warriors,
+    Zombies,
+    Skeleton_Warriors,
+    Black_Knights,
+    Spirit_Host,
+    Hexwraiths
+}
+
+[System.Serializable]
+public class UnitInfo : MonoBehaviour {
+	public List<string> equipment = new List<string> ();
+    public List<string> mount = new List<string>();
+    public List<string> specialRules = new List<string>();
+	public UnitType type;
+	public int	movement,
+				weaponSkill,
+				ballisticSkill,
+				strength,
+				toughness,
+				wounds,
+				initiative,
+				attack,
+				leadership;
+}
