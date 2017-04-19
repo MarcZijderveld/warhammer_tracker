a1c733778065be613ff0810f93285986e0578278
Marc Zijderveld <marczijderveld@planet.nl>
2017-04-19 11:09:38 +0200

    Units and manager

---
+++ Warhammer Tracker/Assets/UnitInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UnitType {
+	Phoenix_Guard,
+	Sisters_of_Avalorn
+}
+
+[System.Serializable]
+public class UnitInfo : MonoBehaviour {
+	public List<string> equipment = new List<string> ();
+	public List<string> specialRules = new List<string>();
+	public UnitType type;
+	public int	movement,
+				weaponSkill,
+				ballisticSkill,
+				strenght,
+				toughness,
+				wounds,
+				initiative,
+				attack,
+				leadership;
+}
---
+++ Warhammer Tracker/Assets/UnitManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour {
+
+	public List<UnitInfo> units = new List<UnitInfo>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
