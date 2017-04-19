5f074a6fac0ea766398aa2ed8fbc5251c54106d2
DennievanderStarre <dennievanderstarre@hotmail.com>
2017-04-19 16:56:02 +0200

    Fix repo fckiup:

--- Warhammer_Tracker/Assets/Scripts/ColorPicker.cs
+++ Warhammer_Tracker/Assets/Scripts/ColorPicker.cs
@@ -3,43 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vexpot.Integration;
 
-public class ColorPicker : MonoBehaviour {
+public class ColorPicker : MonoBehaviour
+{
 
-	private WebCamTexture _texture;
-	public 	VideoCaptureRenderer videoRenderer;
-	public 	ColorTrackerPanel ctp;
+    private WebCamTexture _texture;
+    public VideoCaptureRenderer videoRenderer;
+    public ColorTrackerPanel ctp;
 
-	// Use this for initialization
-	void Start () {
-		_texture = videoRenderer.getWebCamTexture ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!_texture) {
-			_texture = videoRenderer.getWebCamTexture ();
-			Debug.Log (_texture.width);
-		}
-		if(Input.GetMouseButtonDown(1))
-		{
-			RaycastHit hit;
-			if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
-				return;
+    // Use this for initialization
+    void Start()
+    {
+        _texture = videoRenderer.getWebCamTexture();
+    }
 
-			Renderer rend = hit.transform.GetComponent<Renderer>();
-			MeshCollider meshCollider = hit.collider as MeshCollider;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_texture)
+        {
+            _texture = videoRenderer.getWebCamTexture();
+            Debug.Log(_texture.width);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
 
-			if (rend == null || meshCollider == null)
-				return;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-			Vector2 pixelUV = hit.textureCoord;
-			pixelUV.x *= _texture.width;
-			pixelUV.y *= _texture.height;
+            MeshCollider collie = videoRenderer.gameObject.GetComponentInChildren<MeshCollider>();
 
-			Color32 color = (Color32)_texture.GetPixel ((int)pixelUV.x, (int)pixelUV.y);
-			ctp.colorTargets.Add (new Vexpot.ColorTarget (color));
-			ctp.UpdateColorTargets ();
-		 	return;
-		}
-	}
+            if (!collie.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 2000f))
+                return;
+
+            Debug.DrawRay(ray.origin, ray.direction * 5000000, Color.green);
+            //Debug.LogError("Stop!");
+
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+            MeshCollider meshCollider = hit.collider as MeshCollider;
+
+            if (rend == null || meshCollider == null)
+                return;
+
+            Vector2 pixelUV = hit.textureCoord;
+            Debug.Log(pixelUV);
+            pixelUV.x *= 1280f;
+            pixelUV.y *= 720f;
+
+            Color32 color = (Color32)_texture.GetPixel((int)pixelUV.x, (int)pixelUV.y);
+            ctp.colorTargets.Add(new Vexpot.ColorTarget(color, 20f));
+            ctp.StopColorTracker();
+            ctp.UpdateColorTargets();
+            ctp.StartColorTracker();
+            return;
+        }
+    }
 }
